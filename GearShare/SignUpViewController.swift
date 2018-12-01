c1f95e1cd3da9453578e77f9b70a8e1ba7384538
joeyoung33333 <joe@therealmacgenius.com>
2018-12-01 18:04:12 -0500

    commiting pods

---
+++ GearShare/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  GearShare
+//
+//  Created by Joe Young on 12/1/18.
+//  Copyright © 2018 cs.nyu.edu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+    // HELLO
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
