f290545e887d3545b1580a87187604bdf655fdb4
Larry Liu <lawrence@172-18-0-27.dynapool.nyu.edu>
2018-12-18 13:05:42 -0500

    map pins

--- GearShare/MapViewController.swift
+++ GearShare/MapViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
-
+    
     @IBOutlet weak var map: MKMapView!
+    var db = Firestore.firestore()
     var locationManager = CLLocationManager()
+    var addresses = [String]()
+    var userID: String!
+    var address = ""
     
     let geocoder = CLGeocoder()
     let regionRadius: CLLocationDistance = 2414.02
@@ -37,25 +44,111 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             self.locationManager.startUpdatingLocation()
         }
         
-        // GET ADDRESS FROM FIREBASE
-        let address = "80 Lafayette St, New York, NY 10013"
-        let geoCoder = CLGeocoder()
-        geoCoder.geocodeAddressString(address) { (placemarks, error) in
-            guard
-                let placemarks = placemarks,
-                let location = placemarks.first?.location
-                else {
-                    // handle no location found
-                    return
+        // get all addresses except for current user's
+        db.collection("users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let docData = document.data()
+                    let add = docData["address"] as! String
+                    if add != self.address {
+                        self.addresses.append(add)
+                        print(add)
+                    }
+                }
+                for location in self.addresses {
+                    let ann = MKPointAnnotation()
+                    ann.title = location
+                    let geoCoder = CLGeocoder()
+                    geoCoder.geocodeAddressString(location) { (placemarks, error) in
+                        guard
+                            let placemarks = placemarks,
+                            let location = placemarks.first?.location
+                            else {
+                                // handle no location found
+                                return
+                        }
+                        ann.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                        self.map.addAnnotation(ann)
+                    }
+                }
+                
             }
-            
-            self.centerMapOnLocation(location: location)
-            self.annotation.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            self.annotation.title = "Current Location"
-            self.map.addAnnotation(self.annotation)
         }
+        
+        // get current user's address
+        let user = Auth.auth().currentUser
+        if let user = user {
+            let userUID = user.uid
+            userID = user.uid
+            db.collection("users").whereField("userID", isEqualTo: userUID)
+                .getDocuments() { (querySnapshot, error) in
+                    if let error = error {
+                        print("Error getting documents from authenticated user: \(error)")
+                        let alertController = UIAlertController(title: "Error!", message: error.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        for document in querySnapshot!.documents {
+                            let docData = document.data()
+                            self.address = (docData["address"] as! String)
+                            print(self.address)
+                            let geoCoder = CLGeocoder()
+                            geoCoder.geocodeAddressString(self.address) { (placemarks, error) in
+                                guard
+                                    let placemarks = placemarks,
+                                    let location = placemarks.first?.location
+                                    else {
+                                        // handle no location found
+                                        return
+                                }
+                                
+                                self.centerMapOnLocation(location: location)
+                                self.annotation.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                                self.annotation.title = "Current Location"
+                                self.map.addAnnotation(self.annotation)
+                            }
+                        }
+                    }
+            }
+        } else {
+            print("Unable to validate user and query their gear")
+        }
+        
         map.showsUserLocation = true
         //        centerMapOnLocation(location: location)
+    }
+    
+    // When user taps on the disclosure button you can perform a segue to navigate to another view controller
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            print("go to bed")
+            
+            //Perform a segue here to navigate to another viewcontroller
+            // On tapping the disclosure button you will get here
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if !(annotation is MKUserLocation) {
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: String(annotation.hash))
+
+            let rightButton = UIButton(type: .contactAdd)
+            rightButton.tag = annotation.hash
+
+            pinView.animatesDrop = true
+            pinView.canShowCallout = true
+            pinView.rightCalloutAccessoryView = rightButton
+
+            return pinView
+        }
+        else {
+            return nil
+        }
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
@@ -72,4 +165,5 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     }
     
 }
+
 
