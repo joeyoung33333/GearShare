eb0eb34fd50443079b57d8bc2ebb420b937cb4ea
Larry Liu <lawrence@10-17-120-242.dynapool.wireless.nyu.edu>
2018-12-19 15:08:56 -0500

    commented map file

--- GearShare/MapViewController.swift
+++ GearShare/MapViewController.swift
@@ -67,10 +67,10 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                         // error trapping for not being able to get documents
                         print("Error getting documents from authenticated user: \(error)")
                         let alertController = UIAlertController(title: "Error!", message: error.localizedDescription, preferredStyle: .alert)
-                        
+                        // alert for the error
                         let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
                         alertController.addAction(defaultAction)
-                        
+                        // present the alert controller if there's an error
                         self.present(alertController, animated: true, completion: nil)
                     } else {
                         for document in querySnapshot!.documents {
@@ -87,6 +87,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                                         // handle no location found
                                         return
                                 }
+                                // center the map on the user's address
                                 self.centerMapOnLocation(location: location)
                             }
                             // get all addresses except for current user's
@@ -108,10 +109,14 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                                         let ann = MKPointAnnotation()
                                         // split the address for the map annotation
                                         var fullLocationArr = location.split{$0 == ","}.map(String.init)
+                                        // the title will be the number and street address
                                         ann.title = fullLocationArr[0]
+                                        // halfAdd is the second half of the address which will be the subtitle
                                         let halfAdd = fullLocationArr[1] + "," + fullLocationArr[2]
+                                        // trim the whitespace from the front to clean up formatting
                                         let trimmedString = halfAdd.trimmingCharacters(in: .whitespacesAndNewlines)
                                         ann.subtitle = trimmedString
+                                        // get latitude and longitudes for all other addresses
                                         let geoCoder = CLGeocoder()
                                         geoCoder.geocodeAddressString(location) { (placemarks, error) in
                                             guard
@@ -121,7 +126,9 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                                                     // handle no location found
                                                     return
                                             }
+                                            // set coordinates for the annotations
                                             ann.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                                            // add the annotations to the map
                                             self.map.addAnnotation(ann)
                                         }
                                     }
@@ -131,69 +138,90 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                     }
             }
         } else {
+            // error trapping
             print("Unable to validate user and query their gear")
         }
-        
+        // show the user's location
         map.showsUserLocation = true
         //        centerMapOnLocation(location: location)
     }
     
     // segue when you tap on the button for the pin
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        // if the button pressed is the right callout button then trigger this conditional
         if control == view.rightCalloutAccessoryView {
+            // reformat address to align with addresses stored in the database
             let current = view.annotation!.title!! + ", " + view.annotation!.subtitle!!
+            // if the address isn't the current one
             if current != address {
+                // create a new storyboard based on the tableviewcontroller file
                 let Storyboard = UIStoryboard(name: "Main", bundle: nil)
+                // create new view controller based on the products at the given address
                 let vc = Storyboard.instantiateViewController(withIdentifier: "AvaliableProducts") as! TableViewController
                 db.collection("items").whereField("address", isEqualTo: current).getDocuments() { (querySnapshot, err) in
-                    print("All Documents")
                     if let err = err {
+                        // error handling
                         print("Error getting documents: \(err)")
                     } else {
+                        // for each document, add it to the corresponding array in the other file
                         for document in querySnapshot!.documents {
+                            // get all the product names at the address
                             let docData = document.data()
                             let itemName = docData["item_name"] as! String
                             vc.products.append(itemName)
-                            print(itemName)
+                            
+                            // get their corresponding prices
                             let priceStr = docData["price_per_day"] as! String
-                            print(priceStr)
                             vc.prices.append("$" + priceStr)
+                            
+                            // get their corresponding conditions
                             let conditionStr = docData["item_condition"] as! String
-                            print(conditionStr)
                             vc.condition.append(conditionStr)
+                            
+                            // get their corresponding document details
                             let uid = docData["owner_UID"] as! String
                             let documentName = "\(String(describing: uid))-\(String(describing: itemName))"
                             vc.documents.append(documentName)
-                            print(documentName)
                         }
+                        // present the new view controller
                         self.present(vc, animated: true, completion: nil)
                     }
                 }
             }
-            print("map pin clicked")
+            // don't need to error trap the else case because there's no annotation for the user's address
         }
     }
     
+    // handle the pins on the map
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if !(annotation is MKUserLocation) {
+            // create a pin view from the original annotations
             let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: String(annotation.hash))
+            // set button size
             let smallRect = CGSize(width: 122, height: 22)
             let rightButton = UIButton(frame: CGRect(origin: .zero, size: smallRect))
+            
+            // set features of the button on the right
             rightButton.tag = annotation.hash
             rightButton.backgroundColor = UIColor(hue: 0.55, saturation: 1, brightness: 0.89, alpha: 1.0)
             rightButton.layer.cornerRadius = 5
             rightButton.layer.borderWidth = 1
             rightButton.layer.borderColor = UIColor.black.cgColor
+            
+            // add a label to provide text for the button
             let label = UILabel(frame: CGRect(x: -40, y: 3, width: 200, height: 15))
             label.textAlignment = .center
             label.text = "View Products"
             label.textColor = .white
-            rightButton.addSubview(label)
             
+            // add the label to the button
+            rightButton.addSubview(label)
+            // animate pin drop upon app start
             pinView.animatesDrop = true
+            // show the "View Products" button on the right
             pinView.canShowCallout = true
+            // set the button press to trigger the earlier function
             pinView.rightCalloutAccessoryView = rightButton
-
             return pinView
         }
         else {
@@ -201,11 +229,14 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         }
     }
     
+    // manage the location on the map
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         let location = locations.last as! CLLocation
+        // center the map around given latitude and longitude
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         var region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
         region.center = map.userLocation.coordinate
+        // set correct region on the map based on prior information
         map.setRegion(region, animated: true)
     }
     
@@ -213,7 +244,4 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
-
-
